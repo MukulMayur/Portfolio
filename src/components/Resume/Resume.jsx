1121794e69034bac08af997b1fb98361cde6f5ee
Mukul <mukulmayur000@gmail.com>
2024-09-11 19:32:34 +0530

    Update Resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 
+const pdf = "https://mukulmayur.github.io/Portfolio/Resume.pdf";
+
 function Resume() {
+  const downloadFile = (url) => {
+    const aTag = document.createElement("a");
+    aTag.href = url;
+    aTag.setAttribute("download", "Mukul_Resume.pdf");
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="text-center">
         <h2 className="text-2xl font-bold mb-4 text-white">
           Download My Resume
         </h2>
-        <a
-          href="/Resume.pdf" // This should be in the public folder
-          download="Mukul_Mayur_Resume.pdf" // Name of the downloaded file
+        <button
+          onClick={() => downloadFile(pdf)}
           className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition-all"
         >
-          Download PDF
-        </a>
+          Download Resume
+        </button>
       </div>
     </div>
   );
