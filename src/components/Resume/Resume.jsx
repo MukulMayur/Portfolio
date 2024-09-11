19ffae78fad32fbc1637bfddf9cf585be9e16575
Mukul <mukulmayur000@gmail.com>
2024-09-11 19:05:44 +0530

    Update:Resume Section

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Resume() {
-  return <div>Resume</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-center">
+        <h2 className="text-2xl font-bold mb-4 text-white">
+          Download My Resume
+        </h2>
+        <a
+          href="/Resume.pdf" // This should be in the public folder
+          download="Mukul_Mayur_Resume.pdf" // Name of the downloaded file
+          className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition-all"
+        >
+          Download PDF
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Resume;
