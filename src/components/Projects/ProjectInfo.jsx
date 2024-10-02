9abc1cf04f28949e6fdbdcefabafd11773fd4893
Mukul <mukulmayur000@gmail.com>
2024-10-02 14:40:47 +0530

    Add Project

--- src/components/Projects/ProjectInfo.jsx
+++ src/components/Projects/ProjectInfo.jsx
@@ -1,6 +1,7 @@
 import github_img from "../../assets/TechIcons/github.svg";
 import Project_1 from "../../assets/Projects_Img/Project_1.jpg";
 import Project_2 from "../../assets/Projects_Img/Project_2.jpg";
+import Project_3 from "../../assets/Projects_Img/Project_3.jpg";
 
 export const Projects_Info = [
   {
@@ -13,25 +14,26 @@ export const Projects_Info = [
     github: "https://github.com/MukulMayur/Portfolio",
     demo: "https://mukulmayur.github.io/Portfolio/",
   },
+
   {
     id: 2,
-    title: "Quickbite",
+    title: "Dashboard",
     description:
-      "This responsive application includes key features such as a Menu, Cart, and Contact Us sections, along with a user-friendly signup page and a secure payment information page. It emphasizes interactivity and a visually appealing design, enhancing the overall user experience.",
+      "Built a Transport admin dashboard with real-time route tracking, improving efficiency by 30%. Designed revenue, vehicle, and driver overviews using Chart.js for better data insights. Created a React.js paginated table for efficient driver management and task tracking.",
     project_img: Project_2,
     github_img: github_img,
-    github: "https://github.com/MukulMayur/Quickbite",
-    demo: "https://mukulmayur.github.io/Quickbite/",
+    github: "https://github.com/MukulMayur/Dashboard",
+    demo: "https://mukulmayur.github.io/Dashboard/",
   },
   {
     id: 3,
-    title: "Portfolio",
+    title: "Quickbite",
     description:
-      "Portfolio project using React, Tailwind CSS, and JavaScript to highlight my skills and projects. It includes sections like About, Projects, and GitHub, with a focus on responsive design and smooth navigation. This project demonstrates my ability to create engaging, functional web interfaces.",
-    project_img: Project_1,
+      "This responsive application includes key features such as a Menu, Cart, and Contact Us sections, along with a user-friendly signup page and a secure payment information page. It emphasizes interactivity and a visually appealing design, enhancing the overall user experience.",
+    project_img: Project_3,
     github_img: github_img,
-    github: "https://github.com/MukulMayur/Portfolio",
-    demo: "https://mukulmayur.github.io/Portfolio/",
+    github: "https://github.com/MukulMayur/Quickbite",
+    demo: "https://mukulmayur.github.io/Quickbite/",
   },
   {
     id: 4,
